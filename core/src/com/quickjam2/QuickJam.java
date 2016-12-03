ac6a77df1a6722d28415c9adb6c253ca4f72aa37
Declan Whitford Jones <wjdeclan@gmail.com>
2016-12-03 15:43:40 -0500

    Obstacle processing/Gravity flipping

--- core/src/com/quickjam2/QuickJam.java
+++ core/src/com/quickjam2/QuickJam.java
@@ -1,34 +1,67 @@
 package com.quickjam2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
 
 public class QuickJam extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	PlayerController player;
+	
+	Texture obstacle;
+
+	HashMap<Integer, Sprite> time = new HashMap<Integer, Sprite>();
 
 	float obstaclesPerSecond = 3;
 	float timeToObstacle;
-	float currentTime;
+	float currentObstacleTime;
+
+	float timeToGravity;
+	float currentGravityTime;
 
 	ArrayList<VectorizedSprite> obstacles = new ArrayList<VectorizedSprite>();
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
+
 		Texture img = new Texture("badlogic.jpg");
 		player = new PlayerController(img);
-		
+
+		time.put(1, new Sprite(new Texture("one.png")));
+		time.put(2, new Sprite(new Texture("two.png")));
+		time.put(3, new Sprite(new Texture("three.png")));
+
+		time.get(1).setScale(5);
+		time.get(1).setPosition(384 - time.get(1).getWidth() / 2, 278 - time.get(1).getHeight() / 2);
+		time.get(2).setScale(5);
+		time.get(2).setPosition(384 - time.get(2).getWidth() / 2, 278 - time.get(2).getHeight() / 2);
+		time.get(3).setScale(5);
+		time.get(3).setPosition(384 - time.get(3).getWidth() / 2, 278 - time.get(3).getHeight() / 2);
+
 		timeToObstacle = 1 / obstaclesPerSecond;
-		currentTime = 0;
+		currentObstacleTime = -3;
+
+		timeToGravity = 4;
+		currentGravityTime = -3;
 
 		Gdx.input.setInputProcessor(this);
+	}
+	
+	public void reset() {
+		timeToObstacle = 1 / obstaclesPerSecond;
+		currentObstacleTime = -3;
+
+		timeToGravity = 4;
+		currentGravityTime = -3;
 	}
 
 	@Override
@@ -36,10 +69,55 @@ public class QuickJam extends ApplicationAdapter implements InputProcessor {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		float dt = Gdx.graphics.getDeltaTime();
+
+		currentObstacleTime += dt;
+		currentGravityTime += dt;
+
+		if (currentObstacleTime > timeToObstacle) {
+			obstacles.add(new VectorizedSprite(obstacle));
+			currentObstacleTime -= timeToObstacle;
+		}
+		
+		if (currentGravityTime > timeToGravity) {
+			player.flip(false, true);
+			player.flipGravity();
+		}
+
+		ArrayList<VectorizedSprite> toRemove = new ArrayList<VectorizedSprite>();
+
+		for (VectorizedSprite vs : obstacles) {
+			if (vs.getX() > 775) {
+				toRemove.add(vs);
+			}
+			if (Intersector.overlaps(player.getBoundingRectangle(), vs.getBoundingRectangle())) {
+				reset();
+			}
+		}
+
+		for (VectorizedSprite vs : toRemove) {
+			obstacles.remove(vs);
+		}
+
 		batch.begin();
 		
+		for (VectorizedSprite vs : obstacles) {
+			vs.draw(batch);
+		}
+
 		player.draw(batch);
 		
+		if ((currentGravityTime > timeToGravity - 3 && currentGravityTime < timeToGravity - 2.5)
+				|| (currentGravityTime > -3 && currentGravityTime < -2.5)) {
+			time.get(3).draw(batch);
+		} else if ((currentGravityTime > timeToGravity - 2 && currentGravityTime < timeToGravity - 1.5)
+				|| (currentGravityTime > -2 && currentGravityTime < -1.5)) {
+			time.get(2).draw(batch);
+		} else if ((currentGravityTime > timeToGravity - 1 && currentGravityTime < timeToGravity - 0.5)
+				|| (currentGravityTime > -1 && currentGravityTime < -0.5)) {
+			time.get(1).draw(batch);
+		}
+
 		batch.end();
 	}
 
